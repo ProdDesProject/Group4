990f281b147574e69cdabd6e04498fd6999a8b51
Pyry Viirret <pyry.viirret@gmail.com>
2020-11-04 19:04:38 +0200

    new api with a working test for database

---
+++ API_DB/API/routers/users.js
@@ -0,0 +1,96 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+const bcrypt = require('bcryptjs');
+let router = express.Router();
+const {
+    v4: uuidv4
+} = require('uuid');
+
+const users2 = require('./users2');
+const db = require('./db');
+
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
+
+/*let Userdata = [
+    {
+        userId: 0,
+        username: "Kilpikalevi",
+        name: "Taneli",
+        password: '$2y$06$PhZ74dT8/5g6B8SgssFq6ey4ojLxmP6pos2DcevMUGw25Vc9jGEou', //testerpassword
+        email: "Apina@gmail.com",
+        phoneNumber: "01234"
+    }
+  ];*/
+  
+  /*
+  let userObject = {
+        "userId": 0,
+        "username": "Kilpikalevi",
+        "name": "Taneli",
+        "password": "$2y$06$PhZ74dT8/5g6B8SgssFq6ey4ojLxmP6pos2DcevMUGw25Vc9jGEou",
+        "email": "Apina@gmail.com",
+        "phoneNumber": "01234"
+  };*/
+
+//toimii
+router
+.route('')
+.get(
+    //passport.authenticate('basic', { session: false }),
+    (req, res) => {
+    db.query('SELECT * FROM users;').then(results => {
+        res.json({ user: results})
+    })
+    .catch(() => {
+        res.sendStatus(500);
+    })
+    /*let user = users2.getAllUsers()
+    res.json({user});*/
+});
+  
+router
+.route('/createuser')
+.post(
+    //passport.authenticate('basic', { session: false }),
+    (req, res) => {
+
+      const hashedPassword = bcrypt.hashSync(req.body.password, 6);
+      console.log(hashedPassword, req.body.username, req.body.name, req.body.email, req.body.phoneNumber);
+      users2.addUser(
+          req.body.username,
+          hashedPassword,
+          req.body.name,
+          req.body.email,
+          req.body.phoneNumber
+          )
+      res.sendStatus(201);
+  });
+
+
+router
+  .route('/createusertest')
+  .post(
+      //passport.authenticate('basic', { session: false }),
+      (req, res) => {
+        db.query('INSERT INTO users (username,password,name,email,phoneNumber)VALUES ("Tester","password123","Seppo","seppo@gmail.com","+35812123123");');
+        //const hashedPassword = bcrypt.hashSync(req.body.password, 6);
+        res.sendStatus(201);
+    });
+
+router
+.route('/delete/:userId')
+.delete(
+    //passport.authenticate('basic', { session: false }),
+    (req, res) => {
+        db.query('DELETE FROM users WHERE userId = ?',[req.params.userId]);
+        db.query('ALTER TABLE users AUTO_INCREMENT=?',[(req.params.userId-1)]); 
+        /*let Itemid = req.params;
+        console.log(Itemid.id);
+        console.log(ItemsData[Itemid.id]);
+        ItemsData.splice(Itemid.id,ItemsData.length);
+        console.log(ItemsData);*/
+        res.sendStatus(200);
+    })
+  
+  module.exports = router;
