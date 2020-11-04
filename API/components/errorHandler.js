21a7dddc90f9bdd48971b63e630e16a2dcf80f13
Pyry Viirret <pyry.viirret@gmail.com>
2020-11-04 17:17:39 +0200

    removed folder to make a new template

--- API/components/errorHandler.js
+++
@@ -1,8 +0,0 @@
-  module.exports = {
-      clientError: function (err, req, res, next) {
-          res.status(400).json({
-              "error": err.message
-          });
-          next(err);
-      }
-  }
