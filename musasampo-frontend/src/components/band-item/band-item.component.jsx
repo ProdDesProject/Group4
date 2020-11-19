b33b1fc1df165c7ea87a86f82463dbe7a05c3c21
HauptschuIe <45045566+HauptschuIe@users.noreply.github.com>
2020-11-19 19:21:22 +0200

    add band search and fix css

---
+++ musasampo-frontend/src/components/band-item/band-item.component.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { withRouter } from 'react-router-dom';
+
+import CustomButton from '../custom-button/custom-button.component';
+
+
+import './band-item.styles.scss';
+
+const BandItem = ({ band, history }, props) => {
+    const { bandId, nsfw, bandName, bandLogo } = band;
+
+    return (
+        <div className='band-item'>
+            <div
+                className='image'
+                style={{
+                    backgroundImage: `url(${bandLogo})`
+                }}
+            />
+            <div className='band-footer'>
+                <span className='bandInformation'>{bandName}</span>
+            </div>
+            <div className='band-footer'>
+                <span className='name'> Artist</span>
+            </div>
+            <CustomButton onClick={() => {
+                history.push(`/shop/bands/${bandId}`)
+            }} inverted>
+                BAND INFORMATION
+      </CustomButton>
+
+        </div>
+    );
+};
+
+export default withRouter(BandItem);
