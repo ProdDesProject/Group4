070bc2e8e4a65d72a9ede0af6442edb56a219b40
HauptschuIe <45045566+HauptschuIe@users.noreply.github.com>
2020-12-13 23:05:07 +0200

    clean up code and add comments

--- musasampo-frontend/src/pages/chat/chat.component.jsx
+++ musasampo-frontend/src/pages/chat/chat.component.jsx
@@ -13,6 +13,7 @@ import './chat.styles.scss';
 
 import functions from './functions.js'
 
+// address of the websocket
 const URL = 'ws://193.196.53.33:3030';
 
 const ChatPage = () => {
@@ -27,9 +28,9 @@ const ChatPage = () => {
     const [messages, setMessages] = useState([]);
     const [selectedChannel, setSelectedChannel] = useState('');
 
-    const handleChange = (event) => {
-        setSelectedChannel(event.target.value);
-    };
+
+
+    // get all channels
 
     const getChannels = async () => {
         try {
@@ -41,33 +42,7 @@ const ChatPage = () => {
         }
     };
 
-    const createUserChannels = async (username, channelname) => {
-        try {
-            const body = { username, channelname };
-            const response = await fetch("http://193.196.53.33:3000/userchannels", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(body)
-            });
-            const jsonData = await response.json();
-            console.log("User channel connection created: " + JSON.stringify(jsonData));
-        } catch (err) {
-            console.error(err.message);
-        }
-    };
-
-    const getUserByUsername = async (username) => {
-        try {
-            const response = await fetch(`http://193.196.53.33:3000/users/${username}`, {
-                method: "GET"
-            });
-            const jsonData = await response.json();
-            // DO SOMETHING
-            console.log(jsonData)
-        } catch (err) {
-            console.error(err.message);
-        }
-    };
+    // get channels by username
 
     const getUserChannels = async (username) => {
         try {
@@ -76,13 +51,12 @@ const ChatPage = () => {
             });
             const jsonData = await response.json();
             setSelectedChannels(jsonData);
-            console.log(jsonData)
         } catch (err) {
             console.error(err.message);
         }
     };
 
-    //delete function
+    // delete user-channel connection
 
     const deleteUserChannels = async (username, channelname) => {
         try {
@@ -99,21 +73,7 @@ const ChatPage = () => {
         }
     };
 
-    const createMessage = async (message) => {
-        try {
-            const body = message;
-            console.log(JSON.stringify(body));
-            const response = await fetch("http://193.196.53.33:3000/messages", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(body)
-            });
-            const jsonData = await response.json();
-            console.log("Message added: " + JSON.stringify(jsonData));
-        } catch (err) {
-            console.error(err.message);
-        }
-    };
+    // get all messages by channel
 
     const getMessages = async (channelname) => {
         try {
@@ -121,20 +81,20 @@ const ChatPage = () => {
                 method: "GET"
             });
             const jsonData = await response.json();
-            console.log(jsonData)
             setMessages(jsonData);
-
-
-            console.log(messages)
         } catch (err) {
             console.error(err.message);
         }
     };
 
+    const handleChange = (event) => {
+        setSelectedChannel(event.target.value);
+    };
+
     const submitMessage = (messageString, selectedChannel) => {
         // on submitting the ChatInput form, send the message, add it to the list and reset the input
         const message = { message_id: uuidv4(), messagecontent: messageString, username: userName, channelname: selectedChannel }
-        createMessage(message)
+        functions.createMessage(message)
         ws.send(JSON.stringify(message))
 
     }
@@ -149,16 +109,11 @@ const ChatPage = () => {
             // on receiving a message, add it to the list of messages
             const message = JSON.parse(evt.data)
             setMessages(messages => [...messages, message])
-            console.log('already added')
 
         }
 
         ws.onclose = () => {
             console.log('disconnected')
-            // automatically try to reconnect on connection loss
-            // setState({
-            //ws: new WebSocket(URL),
-            // })
         }
         getMessages(userName)
         getChannels();
@@ -166,16 +121,18 @@ const ChatPage = () => {
         functions.getUserByUsername(userName);
     }, []);
 
-    /* change search field state to search field input  */
+    // change search field state to search field input 
     const onSearchChange = event => {
         setSearchField(event.target.value);
     };
 
+
+    // add user-channel connections
     const handleChangeMultiple = (event) => {
         const channel = channels.filter(channel =>
             channel.channelname.toLowerCase().includes(event.target.value.toLowerCase())
         );
-        createUserChannels(userName, channel[0].channelname)
+        functions.createUserChannels(userName, channel[0].channelname)
 
         setSelectedChannels([...selectedChannels, channel[0]])
     }
@@ -185,8 +142,9 @@ const ChatPage = () => {
         channel.channelname.toLowerCase().includes(searchField.toLowerCase())
     );
 
-    const handleDelete = (chipToDelete) => () => {
+    // delete user-channel connections
 
+    const handleDelete = (chipToDelete) => () => {
         const channelname = chipToDelete.channelname;
         deleteUserChannels(userName, channelname)
     };
